a9f66cb67e3350f2032c273c09411efd35423145
FridaMarieJ <93325755+fridamariejoergensen@users.noreply.github.com>
2024-11-27 14:05:42 +0100

    Update disease_prediction.py

--- prediction/disease_prediction.py
+++ prediction/disease_prediction.py
@@ -331,6 +331,8 @@ if __name__ == '__main__':
 
     print('hyper-parameters:')
     print(args)
+
+    args.epochs = int(args.epchs)
     
     if len(args.random_state.split('-')) == 2:
         rs_min, rs_max = int(args.random_state.split('-')[0]),int(args.random_state.split('-')[1])
